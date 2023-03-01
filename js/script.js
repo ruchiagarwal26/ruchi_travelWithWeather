f451bd2fcd5c125e8a1723009cc85270a4dbd219
Ruchi <contactruchiagarwal@gmail.com>
2023-02-28 22:08:45 -0500

    adding history func

--- js/script.js
+++ js/script.js
@@ -3,24 +3,26 @@ containerCity = document.getElementsByClassName("text-white");
 savedSearch = document.getElementsByClassName("historyCity");
 
 const todayDate = new Date();
-const dateSixDays = todayDate.setDate(todayDate.getDate() + 6);
 
 const newName = document.getElementById("cityInput");
 const cityName = document.getElementById("cityName");
 localStorage.setItem("City: ", newName.value);
 
 function updateWeather(fetchedData, cityWeather) {
-    let data = fetchedData;
-    document.getElementById("city").innerHTML = cityWeather ?? newName.value.toUpperCase();
-    if(!data) {
+  let data = fetchedData;
+  document.getElementById("city").innerHTML =
+    cityWeather ?? newName.value.toUpperCase();
+  if (!data) {
     console.log("cityWeather: ", cityWeather);
     data = JSON.parse(localStorage.getItem(cityWeather.toLowerCase()));
-   // return;
-}
-    console.log(data);
+    // return;
+  } else {
+    searchHistory();
+  }
+
+  console.log(data);
   var daily = "";
   var dateDisplay = todayDate;
-  //document.getElementById("city").innerHTML = newName.value.toUpperCase();
   for (i = 0; i < 42; i = i + 8) {
     if (i == 40) {
       var i = 39;
@@ -33,19 +35,15 @@ function updateWeather(fetchedData, cityWeather) {
     var day = displayDateTimeSting.slice(0, 3);
     dateDisplay.setDate(dateDisplay.getDate() + 1);
 
-    // if (IncrementDayPart = displayDateTime & IncrementDayPart < todayDate.getDate() + 6 ) {
-    //i=0;
     var windSpeed = data.list[i].wind.speed + " MPH ";
     //273.15) × 9/5 + 32 -- kelvin to Fahrenheit conversion
     var temp =
       ((Number(data.list[i].main.temp - 273.15) * 9) / 5 + 32).toFixed(1) +
       " °F";
     var humidity = data.list[i].main.humidity + " %";
-    var date = data.list[i].dt_txt;
     var weatherIcon = data.list[i].weather[0].icon;
     var img = document.createElement("img");
 
-    //console.log("weather", weatherIcon)
     document.getElementById("day" + i).innerHTML = "Day: " + day;
     document.getElementById("date" + i).innerHTML = "Date: " + displayDateTime;
     document.getElementById("day" + i + "Temp").innerHTML = "Temp: " + temp;
@@ -53,24 +51,6 @@ function updateWeather(fetchedData, cityWeather) {
       "Humidity: " + humidity;
     document.getElementById("day" + i + "wind").innerHTML =
       "windspeed: " + windSpeed;
-
-    localStorage.setItem(day, document.getElementById("day" + i).innerHTML);
-    localStorage.setItem(
-      day + " date",
-      document.getElementById("date" + i).innerHTML
-    );
-    localStorage.setItem(
-      day + " Temp",
-      document.getElementById("day" + i + "Temp").innerHTML
-    );
-    localStorage.setItem(
-      day + " humidity",
-      document.getElementById("day" + i + "humidity").innerHTML
-    );
-    localStorage.setItem(
-      day + " wind",
-      document.getElementById("day" + i + "wind").innerHTML
-    );
 
     // "http://openweathermap.org/img/wn/"+ weatherIcon+".png";
     var src = document.getElementById("img" + i);
@@ -123,38 +103,28 @@ function getCityName() {
         })
 
         .then(function (data) {
-          //currentData = JSON.stringify(data);
           localStorage.setItem(city, JSON.stringify(data));
-          console.log("hello");
           updateWeather(data);
         });
     });
 }
 
-var saved_city = localStorage.getItem("City: ");
-var saved_date = localStorage.getItem("day");
-
 //to save search history and display on the page
-
 function searchHistory() {
   const recentSearch = [];
   recentSearch.push(newName.value.toUpperCase());
- // console.log("recentSearch: " + recentSearch);
-  var dataExist = localStorage.getItem(newName.value);
-  console.log("dataExist : ", dataExist);
-  if (dataExist) {
-  for (j = 0; j < recentSearch.length; j++) {
-    const BUTTON = document.createElement("BUTTON");
-    
-    BUTTON.setAttribute("type", "submit");
-    BUTTON.setAttribute("class", "historyCity");
-    BUTTON.setAttribute("id", recentSearch[j]);
-    BUTTON.setAttribute("value", recentSearch[j]);
-    //BUTTON.setAttribute("onclick" ,updateWeather(savedWeather));
-    console.log(recentSearch[j]);
-    BUTTON.addEventListener("click", () => {updateWeather(undefined, recentSearch[0])});
-    BUTTON.innerHTML = recentSearch[j];
-    document.getElementById("history").append(BUTTON);
-  }
-}
+    for (j = 0; j < recentSearch.length; j++) {
+      const button = document.createElement("button");
+
+      button.setAttribute("type", "submit");
+      button.setAttribute("class", "historyCity");
+      button.setAttribute("id", recentSearch[j]);
+      button.setAttribute("value", recentSearch[j]);
+      button.addEventListener("click", () => {
+        updateWeather(undefined, recentSearch[0]);
+      });
+      button.innerHTML = recentSearch[j];
+      document.getElementById("history").append(button);
+    }
+  //}
 }
